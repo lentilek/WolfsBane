5d4490c5c93f5adf6fa9696ecf6a03cffec65555
lentilek <laura.robinska@wp.pl>
2025-03-31 00:08:06 +0200

    Basic Highscore UI

---
+++ WolfsBane/Assets/Scripts/Highscore/HighscoreSystem.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System;
+using UnityEngine;
+
+public class HighscoreSystem : MonoBehaviour
+{
+    public static HighscoreSystem Instance;
+
+    [HideInInspector] public HighscoreData data;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;        
+            DontDestroyOnLoad(this);
+        }
+        SaveData();
+    }
+    public void SaveData()
+    {
+        string json = JsonUtility.ToJson(data, true);
+        File.WriteAllText(Application.dataPath + "/HighscoreData.json", json);
+
+       /* PlayerData playerData = new PlayerData();
+        foreach (SceneConfigsSO s in sceneConfigs)
+        {
+            playerData.currentHighScores.Add(s.currentHighScore);
+            playerData.islocked.Add(s.islocked);
+            playerData.dialogStates.Add(s.isDialogueDone);
+        }
+
+        string json = JsonUtility.ToJson(playerData);
+        string path = Path.Combine(Application.persistentDataPath, "playerData");
+        System.IO.File.WriteAllText(path, json);*/
+    }
+    public void LoadData()
+    {
+        //string path = Path.Combine(Application.persistentDataPath, "HighscoreData");
+        string json = File.ReadAllText(Application.dataPath + "/HighscoreData.json");
+        data = JsonUtility.FromJson<HighscoreData>(json);
+
+        /*if (File.Exists(path))
+        {
+            string json = System.IO.File.ReadAllText(path);
+            PlayerData loadedData = JsonUtility.FromJson<PlayerData>(json);
+
+            for (int i = 0; i < sceneConfigs.Count; i++)
+            {
+                sceneConfigs[i].currentHighScore = loadedData.currentHighScores[i];
+                sceneConfigs[i].islocked = loadedData.islocked[i];
+                sceneConfigs[i].isDialogueDone = loadedData.dialogStates[i];
+            }
+        }
+        else
+        {
+            Debug.Log("File not found");
+        }*/
+    }
+
+    public void ClearSavedData()
+    {
+        //
+    }
+}
